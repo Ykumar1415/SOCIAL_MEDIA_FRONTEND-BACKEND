4d3cbae9fd9ce5e1bff5fe5ba85b433c357cccc9
Yogesh Saini <97088265+Ykumar1415@users.noreply.github.com>
2023-02-11 20:25:40 +0530

    Server Files Added
    Server Files added

---
+++ nodejs_api_socialMedia/routes/auth.js
@@ -0,0 +1,79 @@
+const sgMail = require("@sendgrid/mail");
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+router.post("/register", async (req, res, next) => {
+  try {
+    const hashedpassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = await new User({
+      name: req.body.username || "hek",
+      password: hashedpassword,
+      email: req.body.email,
+      // profilePicture: req.body.profilePicture,
+    });
+    try {
+      await user.save();
+      var transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+          user: "YORG153@outlook.com",
+          pass: "987654321aA@",
+        },
+      });
+      var mailOptions = {
+        from: '"SocialMe" <YORG153@outlook.com>', // sender address
+        to: "yogeshsaini1415@gmail.com", // list of receivers
+        subject: "Success!",
+        template: "You are successfully Signed up", // the name of the template file i.e email.handlebars
+        // text: "Thank you for registering with us. We are glad to have you on board.",
+        html: "<b>Thank you for registering with us. We are glad to have you on board.</b>",
+        context: {
+          name: "Adebola", // replace {{name}} with Adebola
+          company: "My Company", // replace {{company}} with My Company
+        },
+      };
+
+      // trigger the sending of the E-mail
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("Message sent: " + info.response);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.post("/login", async (req, res, next) => {
+  try {
+    const flag = true;
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("Email is not found");
+      flag = false;
+    }
+    const passauthenticate = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passauthenticate) {
+      return res.status(404).send("password not matched");
+      flag = false;
+    }
+    if (flag) {
+    //  localStorage.setItem("user", user.email);
+      return res.status(200).json(user._id);
+
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+module.exports = router;
---
+++ nodejs_api_socialMedia/routes/userpost.js
@@ -0,0 +1,127 @@
+const router = require("express").Router();
+const Userpost = require("../models/userposts");
+const User = require("../models/user");
+const Post = require("../models/userposts");
+const cloudinary = require("cloudinary").v2;
+// cloudinary.config({
+//   cloud_name: "da82eybwn",
+//   api_key: "237953862418852",
+//   api_secret: "1kYg1c5K0IUyoj-UZVXa2akWt-8",
+//   secure: true,
+// });
+// router.post("/create", async (req, res) => {
+//   const Post = new Userpost(req.body);
+//   try {
+//     const savenewpost = await Post.save();
+//     res.send(savenewpost);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
+
+router.put("/update/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      post = new Post(req.body);
+      await post.save();
+      res.send("the post has been updated");
+    } else {
+      res.send("you can update only your post");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+router.put("/create", async (req, res) => {
+  console.log(req.body);
+  // const file = req.body.photo; //input type file ka name = photo_post
+  // cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+  // console.log(result);
+  const post = new Post({
+    userId: req.body.userId,
+    desc: req.body.desc,
+    img: req.body.posturl,
+    likes: [],
+  });
+  try {
+    await post.save();
+    res.send("successFully uploaded and saved");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+// });
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const objid = new Mongo.ObjectID(req.params.id);
+    const post = await Post.findById(objid);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.send("the post has been deleted");
+    } else {
+      res.send("you can delete only your post");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.get("/allPostWithUser", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/like/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get("/myallPosts", async (req, res) => {
+  // const post = await Post.find({}, (err, data) => {
+   let x = "";
+  const posts = await Post.find();
+  if (posts) {
+   
+    const data = posts.map(async (post) => {
+      let usr = await User.findById(post.userId);
+      x = usr.name;
+      console.log(x);
+      return {
+        userName:  x
+      }
+    });
+    res.json(data);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.find({ userId: req.params.id });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
